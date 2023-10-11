5ae8a051947c2ec88f5b4e8cf0b0b42aa962758a
Byron Buyck <bbuyck1@gmail.com>
2023-10-11 13:55:42 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import WaterDropGrid from '/src/components/WaterDropGrid';
+
+function App() {
+
+
+  return (
+    <>
+      <WaterDropGrid />
+        We in the bitch.
+    </>
+  )
+}
+
+export default App
